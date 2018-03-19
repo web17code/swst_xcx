6630fe416c43595c5fd779d09bb4f9a5fe47ec74
web17code <web17code@163.com>
2018-03-19 09:49:29 +0800

    备份

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,18 +1,30 @@
 // pages/list/list.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    isShowPopUp:false,//弹框是否显示
+    pageNum:1,//当前页码
+    cfg:getApp().globalData.cfg,
+    nohaveData:false,//列表是否完成标志
+    listData:[]//列表数据
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    // if (getApp().globalData.userID==null){//请求还没来
+    //   console.log("回调getlistData")
+    //   getApp().listCallBack= ()=>{
+    //     this.getListData();
+    //   }
+    // }else{
+    //   this.getListData();
+    // }
   },
 
   /**
@@ -26,7 +38,20 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    //清空列表
+    this.data.listData = [];
+    //重置页码
+    this.data.pageNum = 1;
+    //重置是否加载数据标志
+    this.data.nohaveData = false;
+    if (getApp().globalData.userID == null) {//请求还没来
+      console.log("回调getlistData")
+      getApp().listCallBack = () => {
+        this.getListData();
+      }
+    } else {
+      this.getListData();
+    }
   },
 
   /**
@@ -40,7 +65,16 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+    // //发送时间数据
+    // console.log("发送链接")
+    // wx.request({
+    //   url: 'http://118.25.46.132:8080/gjwl_sys/sysadmin/userAction_Demo',
+    //   method: "GET",
+    //   success: function (res) {
+    //     console.log(res)
+    //     console.log("0.....0" + res)
+    //   }
+    // })
   },
 
   /**
@@ -54,7 +88,12 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+    //再加载更多列表
+    if (this.data.nohaveData){//没有更多数据，不再请求
+      return false;
+    }else{
+      this.getListData();
+    }
   },
 
   /**
@@ -62,5 +101,88 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  goWork:function(e){
+    if(getApp().globalData.userID==null){
+      getApp().getOid();
+      wx.showToast({
+        title: '重新登陆',
+        icon:'loading'
+      })
+      return false;
+    }
+    console.log(e.currentTarget.dataset.num)
+    console.log(e.currentTarget)
+    if (e.currentTarget.dataset.examstatus==2){//交卷的调解析页面
+     
+    }else{
+      wx.navigateTo({
+        url: '../answer/answer?shiJuanId=' + e.currentTarget.dataset.num + '&timuPageNum=' + e.currentTarget.dataset.pagenum + '&examstatus=' + e.currentTarget.dataset.examstatus
+      })
+    }
+    
+  },
+  showPopup:function(){
+    this.setData({
+      isShowPopUp: true
+    })
+  },
+  hidePopup:function(){
+    this.setData({
+      isShowPopUp:false
+    })
+  },
+  //获取列表数据
+  getListData:function(){
+    console.log(getApp().globalData)
+    wx.showToast({
+      title: '加载数据',
+      icon: "loading",
+      mask: true
+    })
+    //10s后关闭反馈框
+    setTimeout(function () { wx.hideToast(); }, 10000)
+    var that = this;
+    wx.request({
+      url: app.globalData.cfg.cfg.http_ip +'/exam/userExams',
+      method: "POST",
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      data: {
+        userId: getApp().globalData.userID,
+        pageSize:10,
+        pageNum:that.data.pageNum
+      },
+      success: (res) => {
+        wx.hideToast();
+        console.log(res)
+        if (res.data.code == "0000") {//请求正确，设置userID
+          if(res.data.data.length<10){//不在请求数据
+            that.data.nohaveData = true;
+          }else{
+            that.data.pageNum = that.data.pageNum + 1;//页码加一为下一页做准备
+          }
+          var allData = that.data.listData.concat(res.data.data);
+          console.log(allData)
+          that.setData({
+            listData: allData
+          })
+        }
+      }
+    })
+  },
+  reDo:function(e){
+    console.log("重做")
+    // wx.navigateTo({
+    //   url: '../analyze/analyze?shiJuanId=' + e.currentTarget.dataset.num + '&timuPageNum=' + e.currentTarget.dataset.pagenum
+    // })
+  },
+  lookAnalyse:function(e){
+    wx.navigateTo({
+      url: '../analyze/analyze?shiJuanId=' + e.currentTarget.dataset.num + '&timuPageNum=' + e.currentTarget.dataset.pagenum
+    })
   }
+
+
 })
---
+++ utils/cfg.js
@@ -0,0 +1,12 @@
+//var http_ip = "https://mapp.zhunedu.com";
+var http_ip = "http://mapp.zhunedu.com/exam";
+//var http_ip = "http://192.168.1.110/exam";
+//var http_ip = "http://192.168.1.111:8083/exam"
+//var ws_ip = "wss://mapp.zhunedu.com/websocket";
+//var ws_ip = "ws://192.168.1.110/tiku/websocket";
+
+var cfg = {};
+cfg={
+  http_ip: http_ip,
+}
+module.exports.cfg = cfg;
